49a214e6b7adcd65807bffc55162da28590db0ae
kira0x1 <kira.eva@protonmail.com>
2022-03-22 03:17:47 +0300

    converted to typescript

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+require("dotenv").config();
+
+export const riotKey = process.env.RIOT_KEY as string;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-require("dotenv").config();
-const { LolApi, Constants } = require("twisted");
+import * as config from "./config";
+import { LolApi, Constants } from "twisted";
 
-const api = new LolApi(process.env.RIOT_KEY);
+const api = new LolApi(config.riotKey);
 
 async function summonerByNameExample() {
   const res = await api.Summoner.getByName(
     "Hide on bush",
     Constants.Regions.KOREA
   );
-
   console.dir(res.response, { depth: 5 });
 }
 
